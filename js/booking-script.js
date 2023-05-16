67a573243c9048805e32735b71cdeb5151151ed7
Nazifa Khatri <nnkhatri10@gmail.com>
2023-05-16 10:46:28 +0530

    added booking page and menu page

---
+++ js/booking-script.js
@@ -0,0 +1,25 @@
+var date = new Date();
+var tdate = date.getDate();
+var month = date.getMonth()+1;
+if(tdate < 10){
+    tdate = '0' + tdate;
+}
+if(month < 10){
+    month = '0' + month;
+}
+var year = date.getUTCFullYear();
+var minDate = year + "-" + month + "-" + tdate;
+document.getElementById("datepicker").setAttribute('min',minDate);
+console.log(minDate);
+
+var hour = date.getHours();
+var minute = date.getMinutes();
+if(hour < 10){
+    hour = '0' + hour;
+}
+if(minute < 10){
+    minute = '0' + minute;
+}
+var minTime = hour + ":" + minute;
+document.getElementById("timepicker").setAttribute('min',minTime);
+console.log(minTime); 
---
+++ js/fooditem.js
@@ -0,0 +1,44 @@
+const foodItem= [
+    {
+        id: 1,
+        name: 'Burger',
+        rating: 4.5,
+        price: 120,
+        img: 'burgerr.jpg',
+        quantity: 1
+},
+{
+    id: 2,
+        name: 'Vegetable Pizza',
+        rating: 4.8,
+        price: 170,
+        img: 'pizzajpg.jpg',
+        quantity: 1
+},
+{
+    id: 3,
+        name: 'Pasta',
+        rating: 4.5,
+        price: 80,
+        img: 'pasta2.jpg',
+        quantity: 1
+},
+{
+    id: 4,
+        name: 'French Fries',
+        rating: 5,
+        price: 70,
+        img: 'fries.jpg',
+        quantity: 1
+},
+{
+    id: 5,
+        name: 'Fried Rice',
+        rating: 4.4,
+        price: 80,
+        img: 'fried-rice.jpg',
+        quantity: 1
+}
+]
+
+export {foodItem};
